4c799556bedef2a7a2f2a9186febb3c95a9e0c56
davidejones <david@davidejones.com>
2017-04-23 21:24:07 -0700

    pangram attempt

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,9 @@
-def is_pangram():
-    pass
+def is_pangram(sentence):
+    """
+    Determine if a sentence is a pangram.
+    A pangram is a sentence using every letter of the alphabet at least once.
+    
+    :param sentence: The input sentence string
+    :return: True if sentence is a pangram and False if it is not
+    """
+    return len(list(filter(lambda x: x.isalpha(), set(sentence)))) == 26
