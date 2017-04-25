18bc97fdc353cc561916821c6e2982487aa66e95
davidejones <david@davidejones.com>
2017-04-25 09:23:51 -0700

    gigasecond

---
+++ python/gigasecond/gigasecond.py
@@ -0,0 +1,11 @@
+from datetime import timedelta
+
+
+def add_gigasecond(dt):
+    """
+    Calculate the moment when someone has lived for 10^9 seconds.
+    
+    :param dt: Datetime input
+    :return: Datetime output
+    """
+    return dt + timedelta(seconds=1000000000)
